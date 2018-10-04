00fc04528fce127b8501b6266058ff870b351cd1
Angelo I Ryndon <angeloryndon@outlook.com>
2018-10-04 18:13:01 +1300

    initial commit, add solution files

---
+++ CSharpNullObjectPattern/CSharpNullObjectPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpNullObjectPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
